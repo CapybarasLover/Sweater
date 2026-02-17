26ed088213bd8bdec330cedb4288510e1ac4b1ee
FertyTry <merzlikin.0404@mail.ru>
2026-02-17 19:11:40 +0300

    Добавил регистрацию и аутентификацию пользователей

--- src/main/java/com/example/sweater/config/WebSecurityConfig.java
+++ src/main/java/com/example/sweater/config/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.example.sweater.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
@@ -9,20 +11,28 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+import java.util.concurrent.ExecutionException;
 
 @Configuration
 @EnableWebSecurity
 class WebSecurityConfig {
+
+    @Autowired
+    DataSource dataSource;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) {
         // @formatter:off
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/").permitAll() //прописываем пути, которые должны быть доступны без аутентификации
+                        .requestMatchers("/", "/registration").permitAll() //прописываем пути, которые должны быть доступны без аутентификации
                         .anyRequest().authenticated() // остальные - требуем аутентификации
                 )
                 .formLogin((form) -> form
@@ -37,13 +47,29 @@ class WebSecurityConfig {
 
     @Bean
     PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(); //Настройка шифрования паролей
+        return NoOpPasswordEncoder.getInstance(); //Настройка шифрования паролей
     }
 
+//    @Bean
+//    UserDetailsService userDetailsService(PasswordEncoder encoder) { //настраиваем хранилище данных
+//        String password = encoder.encode("pass");
+//        UserDetails user = User.withUsername("u").password(password).roles("USER").build();
+//        return new InMemoryUserDetailsManager(user);
+//    }
+
+    // ГЛАВНОЕ: создаем UserDetailsService бин для JDBC authentication
     @Bean
-    UserDetailsService userDetailsService(PasswordEncoder encoder) { //настраиваем хранилище данных
-        String password = encoder.encode("pass");
-        UserDetails user = User.withUsername("u").password(password).roles("USER").build();
-        return new InMemoryUserDetailsManager(user);
+    UserDetailsService userDetailsService() {
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+
+        // Настраиваем кастомные SQL запросы
+        users.setUsersByUsernameQuery(
+                "select username, password, active from usr where username=?"
+        );
+        users.setAuthoritiesByUsernameQuery(
+                "select u.username, ur.roles from usr as u inner join user_role ur on u.id = ur.user_id where u.username=?"
+        );
+
+        return users;
     }
 }
